d31a5198ddd18c49ba96ded0148fdce0683168ba
wootencl <wootencl@g.cofc.edu>
2016-05-29 17:32:35 -0400

    Integration of babel, browserify, and watchify

---
+++ app/angular-js/index.js
@@ -0,0 +1,14 @@
+import 'babel-polyfill';
+import 'zone.js/dist/zone';
+
+import {provide} from '@angular/core';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {ROUTER_PROVIDERS} from '@angular/router';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+
+import {HelloApp} from './app';
+
+bootstrap(HelloApp, [
+  ROUTER_PROVIDERS,
+  provide(LocationStrategy, { useClass: HashLocationStrategy })
+]).catch(err => console.error(err));
--- app/public/js/app.js
+++
@@ -1,6 +0,0 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    $(document).foundation();
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-  });
-})(window.app || (window.app = {}));
