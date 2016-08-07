d4888dbe5566c7d07874ab3bf36b9a6dac6c3c13
wootencl <wootencl@g.cofc.edu>
2016-08-07 16:16:22 -0400

    Fixing captilization error

--- scripts/BeforeInstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-# Stop the node application if it hasn't already been stopped
-forever stop /var/www/html/cw/dist/server.js || true
-
-tar -vczf /home/ec2-user/backups/cw_backup.$(date +%F_%R).tar.gz /var/www/html/cw
-rm -rf /var/www/html/cw/*
