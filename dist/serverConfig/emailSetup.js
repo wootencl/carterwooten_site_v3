5c096555e1acfda8c3e2e15f3d33132b3be5fde1
Carter Wooten <cw@carterwooten.com>
2017-06-26 23:11:50 -0400

    Email tinkering

---
+++ dist/serverConfig/emailSetup.js
@@ -0,0 +1,9 @@
+var nodemailer = require('nodemailer');
+
+module.exports = nodemailer.createTransport({
+  service: 'Mailgun',
+  auth: {
+    user: 'postmaster@mail.carterwooten.com',
+    pass: process.env.MAILGUN_PASSWORD
+  }
+});
