d02d20fcc5f883447b8a1774236572a08bf0d38e
Carter Wooten <cw@carterwooten.com>
2017-06-26 23:12:02 -0400

    Email tinkering

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,3 +2,6 @@
 
 cd /var/www/html/cw
 npm install -s --only=production
+
+# MailGun Pass
+export MAILGUN_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names MAILGUN_PASSWORD --with-decryption --query Parameters[0].Value)
