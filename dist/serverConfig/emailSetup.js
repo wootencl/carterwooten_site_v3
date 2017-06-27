66942bdc2e51b3f4d54e353e5392cc9460603d66
Carter Wooten <cw@carterwooten.com>
2017-06-26 20:49:24 -0400

    Removal of mailgun secret

--- dist/serverConfig/emailSetup.js
+++
@@ -1,9 +0,0 @@
-var nodemailer = require('nodemailer');
-
-module.exports = nodemailer.createTransport({
-  service: 'Mailgun',
-  auth: {
-    user: 'postmaster@mail.carterwooten.com',
-    pass: '4ff96b2da729f4988cbde00b84a29b47'
-  }
-});
