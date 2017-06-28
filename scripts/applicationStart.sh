dbddb3b68efb9ed4cb3e0a216e3c62b8255c3c2b
Carter Wooten <cw@carterwooten.com>
2017-06-27 22:58:28 -0400

    Fingers crossed last MG tweak

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# MailGun Pass
-export MAILGUN_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names MAILGUN_PASSWORD --with-decryption --query Parameters[0].Value)
-
 cd /var/www/html/cw/dist/
-forever start -s server.js
+MAILGUN_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names MAILGUN_PASSWORD --with-decryption --query Parameters[0].Value) forever start server.js
 service httpd restart
